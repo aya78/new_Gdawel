86b94332ab0ad54d42de8b4d86cefeb8820b2325
aya78 <ayak77431@gmail.com>
2023-01-30 16:28:40 +0200

    new1

--- src/test/java/AddServiceIntoStore/neww.java
+++ src/test/java/AddServiceIntoStore/neww.java
@@ -11,6 +11,37 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.util.List;
+
+/** class1
+ * import java.math.BigDecimal;
+
+ public class Item {
+ private String title ;
+ private BigDecimal price ;
+ private String url ;
+
+ public String getTitle() {
+ return title;
+ }
+ public void setTitle(String title) {
+ this.title = title;
+ }
+ public BigDecimal getPrice() {
+
+ return price;
+ }
+ public void setPrice(BigDecimal price) {
+ this.price = price;
+ }
+ public String getUrl() {
+ return url;
+ }
+ public void setUrl(String url) {
+ this.url = url;
+ }
+ }
+ * **/
+/**class2**/
 public class neww {
     //    public static final String baseURL = "https://www.amazon.sa/-/en/gp/bestsellers/?ref_=nav_em_cs_bestsellers_0_1_1_2";
 
