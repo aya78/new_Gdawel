0c1dbcbef807cb02e5cd7c3e00df9a22be980252
aya78 <ayak77431@gmail.com>
2023-06-06 16:05:10 +0300

    add extent report

--- src/test/java/login/Valid_login_test.java
+++ src/test/java/login/Valid_login_test.java
@@ -1,6 +1,9 @@
 package login;
 
 import Pages.login_Page;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,22 +12,60 @@ import org.testng.annotations.*;
 public class Valid_login_test{
 
     public WebDriver driver;
+    public ExtentReports extentReports;
+    public ExtentTest test;
 
     @Test
     public void Valid_login() throws InterruptedException {
+        extentReports = new ExtentReports("/home/hash-pc-8/Documents/vs_code/Gadawl/ExtentReports/TestReports.html",true);
+
         System.setProperty("webdriver.chrome.driver","/home/hash-pc-8/IdeaProjects/Gadawl/src/test/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("w3c", true);
         options.addArguments("--remote-allow-origins=*");
+        test =extentReports.startTest("verify valid  Login ");
+
         driver = new ChromeDriver(options);
         //        driver = new ChromeDriver();
 //       WebDriverManager.chromedriver().setup();
         driver.manage().window().maximize();
         driver.get("https://gdawel.app/");
+        test.log(LogStatus.PASS,"browser is open and windows is maximized");
+        if(login_Page.click_login(driver).isEnabled())
+        {
+            test.log(LogStatus.PASS,"login page is opened");
+        }else
+            test.log(LogStatus.FAIL,"crash in login page");
         login_Page.click_login(driver).click();
+
+
+
+        if(login_Page.enter_email(driver).isSelected())
+        {
+            test.log(LogStatus.PASS,"Username is entered");
+        }else
+            test.log(LogStatus.FAIL,"fail to find username element");
         login_Page.enter_email(driver).sendKeys("ayak77431@gmail.com");
+
+
+
+        if(login_Page.enter_pass(driver).isSelected())
+        {
+            test.log(LogStatus.PASS,"Password is entered");
+        }else
+            test.log(LogStatus.FAIL,"fail to find password element");
         login_Page.enter_pass(driver).sendKeys(""+123456789);
+
+
+
+        if(login_Page.validlogin(driver).isSelected())
+        {
+            test.log(LogStatus.PASS,"gadawel app is open ");
+        }else
+            test.log(LogStatus.FAIL,"fail to find login button");
         login_Page.validlogin(driver).click();
+        extentReports.endTest(test);
+        extentReports.flush();
         Thread.sleep(2000);
         driver.quit();
 
